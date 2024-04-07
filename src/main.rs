5db894babe9d267ef2ef55f85d05b556229c2cde
Ashish Kumer Shill <ashish@brillmark.com>
2024-04-07 12:10:50 +0600

    Ownership concept

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // ---- ownership rules -----
+    // 1. Each value in Rust has a variable thats called its owner.
+    // 2. There can only be one owner at a time.
+    // 3. When the owner goes out of scope, the value will dropped.
+
+    {
+        //S in not valid here, its not yet declared
+        let s = String::from("hello"); //s is valid from this point forword
+                                       //do stuff with S
+    } //this scope is now over, and is no longer valid
+
+    // // How variable interact
+
+    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = s1.clone(); // Move to shallow copy
+
+    println!("{}, world", s1);
+
+    //function ownership
+    let s = String::from("hello");
+    takes_ownership(s);
+    println!("{}", s);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
